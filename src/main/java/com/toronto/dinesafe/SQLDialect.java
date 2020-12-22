a5d4add18f2d40eddc963a6fd83010decd504079
Logan Corey <logan@coldiron.ca>
2020-12-22 17:21:49 -0500

    Removed unused imports

--- src/main/java/com/toronto/dinesafe/SQLDialect.java
+++ src/main/java/com/toronto/dinesafe/SQLDialect.java
@@ -1,12 +1,12 @@
 package com.toronto.dinesafe;
 
-import java.sql.Types;
-
 import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.dialect.function.VarArgsSQLFunction;
 import org.hibernate.type.StringType;
+
+import java.sql.Types;
 
 public class SQLDialect extends Dialect {
     public SQLDialect() {
