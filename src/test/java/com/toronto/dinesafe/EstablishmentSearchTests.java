df0adc235d423e9fcb887ca1b02690b4f8650952
Logan Corey <logan@coldiron.ca>
2020-12-22 17:22:25 -0500

    Added tests for searching for establishments

--- src/test/java/com/toronto/dinesafe/EstablishmentSearchTests.java
+++ src/test/java/com/toronto/dinesafe/EstablishmentSearchTests.java
@@ -1,6 +1,5 @@
 package com.toronto.dinesafe;
 
-import org.apache.catalina.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,20 +8,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @SpringBootTest
 class EstablishmentSearchTests {
-   /*  @Autowired
-    private IEstablishmentDAO userApi;
+     @Autowired
+    private EstablishmentRepository repository;
 
    @Test
-    public void givenFirstAndLastName_whenGettingListOfUsers_thenCorrect() {
-        List<SearchCriteria> params = new ArrayList<SearchCriteria>();
-        params.add(new SearchCriteria("establishmentName", ":", "SAI-LILA KHAMAN DHOKLA HOUSE"));
+    public void findEstablishmentByName() {
+       EstablishmentSpecificationsBuilder builder = new EstablishmentSpecificationsBuilder();
+       SpecSearchCriteria spec = new SpecSearchCriteria("establishmentName", SearchOperation.EQUALITY, "SAI-LILA KHAMAN DHOKLA HOUSE");
+       List<Establishment> results = repository.findAll(builder
+               .with(spec)
+               .build());
+        assertThat(results, hasSize(greaterThan(1)));
+    }
 
-        List<Establishment> results = userApi.searchEstablishment(params);
-        assertThat(results, hasSize(1));
-    }*/
+    @Test
+    public void findResturantsWithAmountFinedMoreThanZero() {
+        EstablishmentSpecificationsBuilder builder = new EstablishmentSpecificationsBuilder();
+        SpecSearchCriteria spec = new SpecSearchCriteria("amountFined", SearchOperation.GREATER_THAN, 0);
+        SpecSearchCriteria spec1 = new SpecSearchCriteria("'", "establishmentType", SearchOperation.EQUALITY, "Restaurant");
+        List<Establishment> results = repository.findAll(builder
+                .with(spec)
+                .with(spec1)
+                .build());
+        assertThat(results, hasSize(greaterThan(1)));
+    }
 
 }
