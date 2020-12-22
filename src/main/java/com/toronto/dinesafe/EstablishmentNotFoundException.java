f14228e2108f0fe77ca013605da459664e4088e6
Logan Corey <logan@coldiron.ca>
2020-12-22 02:00:51 -0500

    Initial commit for github pages site

--- src/main/java/com/toronto/dinesafe/EstablishmentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.toronto.dinesafe;
-
-public class EstablishmentNotFoundException extends RuntimeException{
-
-    EstablishmentNotFoundException(Long id) {
-        super("Could not find establishment " + id);
-    }
-}
--- src/main/java/com/toronto/dinesafe/EstablishmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.toronto.dinesafe;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface EstablishmentRepository extends JpaRepository<Establishment, Long>, JpaSpecificationExecutor<Establishment> {
-}
